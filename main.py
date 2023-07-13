10b3ef07a0fd7d2bab2cbc1dd5b041b0216027f2
Shubham Kawade <shubham@fornaxhq.co>
2023-07-13 16:08:13 +0530

    added the returns_prime_pipeline

---
+++ main.py
@@ -0,0 +1,37 @@
+import pathlib
+from datetime import datetime, timedelta
+from api.gdrive  import GdriveData
+from service.modify_df import ModifyDataframe
+from service import bigquery_service
+
+
+if __name__ == '__main__':
+    date = datetime.now().strftime('%d-%m-%Y')
+    #gdrive = GdriveData(r"C:\Users\admin\Documents\Fornax\TheWhiteWillow\service_account_gdrive\double-exchange-300905-493cb131488e.json")
+    gdrive = GdriveData(f"{pathlib.Path(__file__).parent.resolve()}\double-exchange-300905-493cb131488e.json")
+
+    client = gdrive.generate_gdrive_client()
+    print(client)
+
+    columns = ['serial_number', 'type', 'status', 'customer_name', 'customer_address'
+        , 'customer_email', 'customer_phone', 'requested_at', 'order_number'
+        , 'order_created_at', 'approved_at'
+        , 'received_at', 'inspected_at', 'archived_at', 'inspection_due_by', 'exchange_with', 'exchange_with_sku'
+        , 'exchange_with_amount', 'difference_amount', 'exchange_order_name', 'additional_amount_captured'
+        , 'payment_gateway_name', 'payment_gateway_transaction_id', 'payment_transaction_date', 'order_payment_gateway'
+        , 'item_name', 'item_quantity', 'item_price', 'sku', 'reason', 'customer_comment', 'pickup_awb'
+        , 'pickup_logistics', 'warehouse_location', 'exchanged_at', 'exchange_order_status', 'refund_status',
+               'requested_refund_mode'
+        , 'actual_refund_mode', 'refunded_at', 'return_fee', 'eligible_refund_amount', 'refunded_amount',
+               'gift_card_number'
+        , 'account_number', 'ifsc_code', 'account_holder_name', 'refund_link', 'shipment_tracking_status',
+               'original_return_method'
+        , 'actual_return_method', 'custom_attributes'
+               ]
+
+    returns_prime_df = gdrive.create_df(client, date, columns)
+    modify_df = ModifyDataframe(returns_prime_df)
+    returns_prime_df = modify_df.add_report_upload_date()
+    returns_prime_df = modify_df.convert_dtype_to_string(returns_prime_df)
+    bigquery_service.save_df_to_table(returns_prime_df, 'double-exchange-300905.production.returns_prime_report_raw')
+
