67f37a96f252e48476ae48d1e4d2b757e4b352b4
Shubham Kawade <shubham@fornaxhq.co>
2023-07-18 12:56:49 +0530

    chnaged deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -10,6 +10,7 @@ create_zip() {
   cd returns_prime_etl || exit
   zip -r ../returns_prime_etl.zip .
   cd ..
+  rm returns_prime_etl_lambda_function.zip
   zip -r -u returns_prime_etl_lambda_function.zip returns_prime_etl.zip connection.py lambda_function.py lambda_handler.py
 }
 
