4708bca18e200771c8689643ae51873d30a9c872
Shubham Kawade <shubham@fornaxhq.co>
2023-07-18 11:51:17 +0530

    modified the yaml file

--- deployment.sh
+++ deployment.sh
@@ -6,6 +6,13 @@ build_python_lib() {
  python setup.py sdist bdist_wheel
 }
 
+create_zip() {
+  cd returns_prime_etl || exit
+  zip -r ../returns_prime_etl.zip .
+  cd ..
+  zip returns_prime_etl_lambda_function.zip returns_prime_etl.zip connection.py main.py
+}
+
 upload_to_s3() {
   curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
   sudo installer -pkg ./AWSCLIV2.pkg -target /
@@ -13,8 +20,8 @@ upload_to_s3() {
   export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
  # Values coming from git secret manager
   export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
-  aws s3 cp dist/*.whl s3://tww-metabase-bucket
-  aws s3 cp main.py s3://tww-metabase-bucket/main.py
+  aws s3 cp returns_prime_etl_lambda_function.zip s3://tww-metabase-bucket
+  #aws s3 cp main.py s3://tww-metabase-bucket/main.py
 }
 
 deploy() {
